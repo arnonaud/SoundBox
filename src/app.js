c1fe2025a5264802ee2fe77c0abb8f254ee40e5f
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-01-20 10:47:00 +0100

    structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
-import {SoundBoxController} from './sounds.box.controller';
 
 angular.module('app', [])
-.controller("soundBoxController",SoundBoxController)
 
 .run( function() {
   console.log('SOUNDBOX !');
