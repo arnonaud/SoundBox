00ce9a26c6a90406c110c8b0531d97242b7fdb9b
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-01-20 10:41:20 +0100

    controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
+import {SoundBoxController} from './sounds.box.controller';
 
 angular.module('app', [])
+.controller("soundBoxController",SoundBoxController)
 
 .run( function() {
   console.log('SOUNDBOX !');
---
+++ src/sounds.box.controller.js
@@ -0,0 +1,5 @@
+export class SoundBoxController{
+    constructor(){
+        
+    }
+}
