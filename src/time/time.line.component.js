4f8f14f516c17697f231e68d91596551d0262391
Noe Pontoizeau <noe.pontoizeau@outlook.com>
2017-01-20 12:44:46 +0100

    maj time

---
+++ src/time/time.line.component.js
@@ -0,0 +1,13 @@
+class TimeLineController {
+    constructor() {
+
+    }
+}
+
+export const TimeLine = {
+    bindings: {
+        case: '<'
+    },
+    template: require('./time.line.component.html'),
+    controllerTimeLineController
+}
