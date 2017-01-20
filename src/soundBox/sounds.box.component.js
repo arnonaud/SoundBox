729629c7e1bc97d6ba82be118f648a5f3ed7d9ba
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-01-20 12:00:44 +0100

    component sound line

--- src/soundBox/sounds.box.component.js
+++ src/soundBox/sounds.box.component.js
@@ -1,6 +1,12 @@
 class SoundBoxController{
-    constructor(){
-        
+    constructor(){}
+    $onInit(){
+        this.sounds = [
+            {sound:'./sounds/crash-acoustic.wav',id:1},
+            {sound:'./sounds/kick-dry.wav',id:2},
+            {sound:'./sounds/openhat-tight.wav',id:3},
+            {sound:'./sounds/tom-rototom.wav',id:4}
+        ]
     }
 }
 export const SoundBox = {
