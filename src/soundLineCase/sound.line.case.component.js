141f69454ed8cf2ccf037330ad0233243ee5a9c0
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-01-20 11:03:53 +0100

    soundcase html

---
+++ src/soundLineCase/sound.line.case.component.js
@@ -0,0 +1,8 @@
+class SoundLineCaseController{
+    constructor(){}
+}
+
+export const SoundLineCase = {
+    template : require('./sound.line.case.component.html'),
+    controller :SoundLineCaseController
+}
